d38ff934fbd0fa311d625cb4164498465af062a1
xiong-xiong <jst@It-palvelut-MacBook-Pro-6.local>
2017-03-26 00:11:38 +0200

    Version 1
    This is first version of Simple font-size JS

---
+++ fontsize.js
@@ -0,0 +1,48 @@
+
+  $( ".font1" ).click(function() {
+  $( "p, ul, ol" ).css( "font-size", "13px");
+  $( "h1" ).css( "font-size", "2.5rem");
+  $( "h2" ).css( "font-size", "2rem");
+  $( "h3" ).css( "font-size", "1.8rem");
+  $( "h4" ).css( "font-size", "1.5rem");
+    $( ".font1" ).css( 'color', "#b89e12");
+  $( ".font2" ).css( 'color', "black");
+  $( ".font3" ).css( 'color', "black");
+  $( ".font4" ).css( 'color', "black");
+ });
+
+  $( ".font2" ).click(function() {
+  $( "p, ul, ol" ).css( "font-size", "17px");
+  $( "h1" ).css( "font-size", "3rem");
+  $( "h2" ).css( "font-size", "2.5rem");
+  $( "h3" ).css( "font-size", "2rem");
+  $( "h4" ).css( "font-size", "1.8rem");
+  $( ".font1" ).css( 'color', "black");
+  $( ".font2" ).css( 'color', "#b89e12");
+  $( ".font3" ).css( 'color', "black");
+  $( ".font4" ).css( 'color', "black");
+ });
+
+  $( ".font3" ).click(function() {
+  $( "p, ul, ol" ).css( "font-size", "20px");
+  $( "h1" ).css( "font-size", "3.2rem");
+  $( "h2" ).css( "font-size", "2.7rem");
+  $( "h3" ).css( "font-size", "2.2rem");
+  $( "h4" ).css( "font-size", "2rem");
+  $( ".font1" ).css( 'color', "black");
+  $( ".font2" ).css( 'color', "black");
+  $( ".font3" ).css( 'color', "#b89e12");
+  $( ".font4" ).css( 'color', "black");
+ });
+
+  $( ".font4" ).click(function() {
+  $( "p, ul, ol" ).css( 'font-size', "24px");
+  $( "h1" ).css( "font-size", "3.5rem");
+  $( "h2" ).css( "font-size", "3rem");
+  $( "h3" ).css( "font-size", "2.8rem");
+  $( "h4" ).css( "font-size", "2.3rem");
+  $( ".font1" ).css( 'color', "black");
+  $( ".font2" ).css( 'color', "black");
+  $( ".font3" ).css( 'color', "black");
+  $( ".font4" ).css( 'color', "#b89e12");
+ });
